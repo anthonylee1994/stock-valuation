68e1bea9340e88a38f3f42d62a076158021d819c
Anthony Lee <anthonylee1994@users.noreply.github.com>
2026-02-11 14:03:53 +0800

    refactor: Update SortButtonGroup component styles for improved clarity and consistency
    - Removed variant prop from ButtonGroup to standardize styling.
    - Adjusted background color classes for buttons to enhance visual distinction based on sort order.

--- src/components/SortButtonGroup.tsx
+++ src/components/SortButtonGroup.tsx
@@ -8,11 +8,11 @@ interface Props {
 export const SortButtonGroup = ({sortOrder, onSortOrderChange}: Props) => {
     return (
         <div className="max-w-[1400px] mx-auto mb-6 max-[640px]:text-center">
-            <ButtonGroup>
-                <Button className={sortOrder === "asc" ? "bg-green-500/80" : "bg-transparent"} variant={sortOrder === "asc" ? "primary" : "outline"} onPress={() => onSortOrderChange("asc")}>
+            <ButtonGroup variant="outline">
+                <Button className={sortOrder === "asc" ? "bg-green-500" : "bg-transparent"} onPress={() => onSortOrderChange("asc")}>
                     由殘到貴 ↑
                 </Button>
-                <Button className={sortOrder === "desc" ? "bg-red-500/80" : "bg-transparent"} variant={sortOrder === "desc" ? "primary" : "outline"} onPress={() => onSortOrderChange("desc")}>
+                <Button className={sortOrder === "desc" ? "bg-red-500" : "bg-transparent"} onPress={() => onSortOrderChange("desc")}>
                     由貴到殘 ↓
                 </Button>
             </ButtonGroup>
